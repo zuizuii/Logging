81b963451dfbf9f8eec0780352503987ff7fbf88
Zuizui <1100tamtam@gmail.com>
2023-04-16 01:16:10 +0700

    + fixed use pro skin + auto enable logging when import

--- Logging/Assets/Plugins/Logging/CHANGELOG.md
+++ Logging/Assets/Plugins/Logging/CHANGELOG.md
@@ -3,16 +3,21 @@
 
 ### This is the first release of *Unity Package Logging*.
 
-*first release*
++ *first release*
 
 ## 0.0.2 (2022-05-14)
 
 ### This is the first release of *Unity Package Logging*.
 
-*fix build*
++ *fix build*
 
 ## 0.0.3 (2022-09-20)
 
-fix get log path
++ fix get log path
+
+## 0.0.4 (2023-04-16)
+
++ fix use pro skin
++ auto enable logging when import
 
 
--- Logging/Assets/Plugins/Logging/Editor/LogPreferencesConfigs.cs
+++ Logging/Assets/Plugins/Logging/Editor/LogPreferencesConfigs.cs
@@ -6,11 +6,10 @@ using UnityEngine;
 
 public class LogPreferencesConfigs
 {
-
-	const string FIRST_TIME = "FIRST_TIME";
+	const string FIRST_TIME = "ENABLE_LOGGING_FIRST_TIME";
 	const string LOG_DEFINE_SYMBOL = "USE_LOG";
 
-	private static bool initialised;
+	static bool initialized;
 	public static bool enableLogging = true;
 
 	public static readonly string[] symbols = new string[]
@@ -19,108 +18,108 @@ public class LogPreferencesConfigs
 	};
 
 	[InitializeOnLoadMethod]
-	static void OnProjectLoadedInEditorFirstTime ()
+	static void OnProjectLoadedInEditorFirstTime()
 	{
-		if (EditorPrefs.GetBool (FIRST_TIME, true))
+		if (EditorPrefs.GetBool(FIRST_TIME, true))
 		{
-			EditorPrefs.SetBool (FIRST_TIME, false);
-			AddDefineSymbols (symbols);
-			Debug.Log ("Log is enable for debug, you can turn off in \"Preferences\"");
+			AddDefineSymbols(symbols);
+			EditorPrefs.SetBool(FIRST_TIME, false);
+			Debug.Log("Log is enable for debug, you can turn off in \"Preferences\"");
 		}
 	}
 
-	[PreferenceItem ("Logging")]
-	static void PreferencesGUI ()
+	[PreferenceItem("Logging")]
+	static void PreferencesGUI()
 	{
 		// Load the preferences
-		if (initialised == false)
+		if (initialized == false)
 		{
-			ReadInitialSettings ();
-			initialised = true;
+			ReadInitialSettings();
+			initialized = true;
 		}
 
-		GUILayout.Space (10);
-		GUILayout.Label ("To remove debug logging when profiling development\nbuilds turn off \"Enable Logging\".");
+		GUILayout.Space(10);
+		GUILayout.Label("To remove debug logging when profiling development\nbuilds turn off \"Enable Logging\".");
 		//		GUILayout.Label ("Debugging is automatically turned off in\nnon-development builds");
-		GUILayout.Label ("Debugging is not use for development builds");
+		GUILayout.Label("Debugging is not use for development builds");
 
-		GUILayout.Space (10);
+		GUILayout.Space(10);
 		GUI.enabled = !EditorApplication.isCompiling;
 
-		enableLogging = EditorGUILayout.Toggle ("Enable Logging", enableLogging);
+		enableLogging = EditorGUILayout.Toggle("Enable Logging", enableLogging);
 
 		GUI.enabled = true;
 
 		if (EditorApplication.isCompiling == true)
 		{
-			GUILayout.Space (10);
-			GUILayout.Label ("Please wait... Editor is currently setting define.");
+			GUILayout.Space(10);
+			GUILayout.Label("Please wait... Editor is currently setting define.");
 		}
 
 		if (GUI.changed == true)
 		{
-			UpdateDebugSettings ();
+			UpdateDebugSettings();
 		}
 	}
 
-	static bool HasDefineSymbol ()
+	static bool HasDefineSymbol()
 	{
 		// Get defines.
 		BuildTargetGroup buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
-		string define = PlayerSettings.GetScriptingDefineSymbolsForGroup (buildTargetGroup);
+		string define = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
 
-		return define.Contains (LOG_DEFINE_SYMBOL);
+		return define.Contains(LOG_DEFINE_SYMBOL);
 	}
 
-	static void ReadInitialSettings ()
+	static void ReadInitialSettings()
 	{
-		if (HasDefineSymbol ())
+		if (HasDefineSymbol())
 			enableLogging = true;
 		else
 			enableLogging = false;
 	}
 
-	static void UpdateDebugSettings ()
+	static void UpdateDebugSettings()
 	{
 		if (enableLogging)
 		{
-			AddDefineSymbols (symbols);
+			AddDefineSymbols(symbols);
 		}
 		else
 		{
-			RemoveDefineSymbols (symbols);
+			RemoveDefineSymbols(symbols);
 		}
 	}
 
 	/// <summary>
 	/// Add define symbols as soon as Unity gets done compiling.
 	/// </summary>
-	static void AddDefineSymbols (string[] defineSymbols)
+	static void AddDefineSymbols(string[] defineSymbols)
 	{
-		string definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup (EditorUserBuildSettings.selectedBuildTargetGroup);
-		List<string> allDefines = definesString.Split (';').ToList ();
-		allDefines.AddRange (defineSymbols.Except (allDefines));
-		PlayerSettings.SetScriptingDefineSymbolsForGroup (
+		string definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+		List<string> allDefines = definesString.Split(';').ToList();
+		allDefines.AddRange(defineSymbols.Except(allDefines));
+		PlayerSettings.SetScriptingDefineSymbolsForGroup(
 			EditorUserBuildSettings.selectedBuildTargetGroup,
-			string.Join (";", allDefines.ToArray ()));
+			string.Join(";", allDefines.ToArray()));
 	}
 
-	static void RemoveDefineSymbols (string[] defineSymbols)
+	static void RemoveDefineSymbols(string[] defineSymbols)
 	{
-		string definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup (EditorUserBuildSettings.selectedBuildTargetGroup);
-		List<string> allDefines = definesString.Split (';').ToList ();
+		string definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+		List<string> allDefines = definesString.Split(';').ToList();
 
 		for (int i = allDefines.Count - 1; i >= 0; i--)
 		{
 			for (int j = 0; j < defineSymbols.Length; j++)
 			{
 				if (allDefines[i] == defineSymbols[j])
-					allDefines.RemoveAt (i);
+					allDefines.RemoveAt(i);
 			}
 		}
 
-		PlayerSettings.SetScriptingDefineSymbolsForGroup (
+		PlayerSettings.SetScriptingDefineSymbolsForGroup(
 			EditorUserBuildSettings.selectedBuildTargetGroup,
-			string.Join (";", allDefines.ToArray ()));
+			string.Join(";", allDefines.ToArray()));
 	}
 }
